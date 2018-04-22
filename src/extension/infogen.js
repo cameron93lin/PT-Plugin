07f4225bbf08761e191c51453110f352df3c1934
Rhilip <rhilipruan@gmail.com>
2018-04-22 16:46:32 +0800

    :sparkles: Info Gen For Steam Completed.

--- src/extension/infogen.js
+++ src/extension/infogen.js
@@ -11,23 +11,18 @@
     <h2></h2>
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2 col-md-10 col-md-offset-1">
-            <div class="input-group">
-                <div class="input-control search-box has-icon-left" id="gen-search-key">
-                    <input id="gen-link" type="search" class="form-control search-input" placeholder="资源名或豆瓣/IMDB/Bangumi站点的链接">
-                    <label for="gen-link" class="input-control-icon-left search-icon"><i class="icon icon-search"></i></label>
-                </div>
-                <div class="btn-group input-group-btn" style="width: 15%;">
+            <div class="input-group" style="width: 60%">
+                <input id="gen-link" type="search" class="form-control search-input" placeholder="资源名或豆瓣/IMDB/Bangumi站点的链接">
+                <div class="input-group-btn">
                     <button type="button" class="btn btn-primary" id="gen-search">查询</button>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                            <span class="caret"></span>
-                            <!--<span class="sr-only">查询</span>-->
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a id="gen-search-bgm">通过Bangumi查询</a></li>
-                            <!--<li><a id="gen-search-steam">通过Steam查询</a></li>-->
-                        </ul>
-                    </div>
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" tabindex="-1">
+                        <span class="caret"></span>
+                        <!--<span class="sr-only">查询</span>-->
+                    </button>
+                    <ul class="dropdown-menu pull-right" role="menu">
+                        <li><a id="gen-search-bgm">通过Bangumi查询</a></li>
+                        <li><a id="gen-search-steam">通过Steam查询</a></li>
+                    </ul>
                 </div>
             </div>
             <hr>
@@ -127,11 +122,10 @@
                     return anchor[0].nextSibling.nodeValue.trim();
                 };
 
-                $.get(subject_url, function (data) {
-                    // 检查对应资源是否存在
-                    if (/<title>页面不存在<\/title>/.test(data)) {
-                        system.showMessage("该链接对应的资源似乎并不存在，你确认没填错");
-                    } else {
+                $.ajax({
+                    type: "get",
+                    url: subject_url,
+                    success: data => {
                         let parser = html_parser(data);
                         system.showMessage("已成功豆瓣获取源页面，开始解析");
 
@@ -292,6 +286,9 @@
                                 system.showErrorMessage("豆瓣其他信息请求失败。");
                             },
                         });  //豆瓣评分，简介，海报，导演，编剧，演员，标签
+                    },
+                    error: () => {
+                        system.showErrorMessage("该链接对应的资源似乎并不存在，你确认没填错");
                     }
                 });
             }   // 豆瓣链接
@@ -302,6 +299,8 @@
                         subject_url = data1["alt"].replace("/movie/","/subject/") + "/";
                         Gen.gen(subject_url);
                     }
+                }).fail(() => {
+                    system.showErrorMessage("不能在豆瓣上找到对应IMDb编号，查询失败。");
                 })
             }  // IMDb 链接
             else if (subject_url.match(/\/\/(bgm\.tv|bangumi\.tv|chii\.in)\/subject/)) {
@@ -360,8 +359,165 @@
                 });
             }  // Bangumi链接
             else if (subject_url.match(/(store\.)?steam(powered|community)\.com\/app\/\d+/)) {
+                // 设置Cookies
+                chrome.cookies.set({
+                    url: "https://store.steampowered.com/",
+                    name: "Steam_Language",
+                    value: "schinese",
+                    path: "/"
+                });  // 设置为中文界面
+                chrome.cookies.set({
+                    url: "https://store.steampowered.com/",
+                    name: "mature_content",
+                    value: "1",
+                    path: "/"
+                });  // 避免 Steam 年龄认证（直接点击类）
 
+                let steam_id = subject_url.match(/app\/(\d+)/)[1];
 
+                function steam_parser(data) {
+                    // 所有需要获取的信息
+                    let cover,cname,name,genre,developer,publisher,date,linkbar,language,tags,review;
+                    let introduce,screenshot,sysreq;
+
+                    // 简介生成方法
+                    function descriptionGenerator() {
+                        let descr = "";
+                        descr += cover ? `[img]${cover}[/img]\n\n` : "";
+                        descr += "【基本信息】\n\n";
+                        descr += cname ? `中文名： ${cname}\n` : "";
+                        descr += name ? `英文名： ${name}\n` : "";
+                        descr += genre ? `类型： ${genre.join(", ")}\n` : "";
+                        descr += developer ? `开发商： ${developer.join(", ")}\n` : "";
+                        descr += publisher ? `发行商： ${publisher.join(", ")}\n` : "";
+                        descr += date ? `发行日期： ${date}\n` : "";
+                        descr += linkbar ? `官网： ${linkbar}\n` : "";
+                        descr += language ? `游戏语种： ${language.join(" / ")}\n` : "";
+                        descr += tags ? `标签： ${tags.join(" / ")}\n` : "";
+                        descr += review ? `${review.join("\n")}\n` : "";
+                        descr += "\n【游戏简介】\n\n";
+                        descr += introduce ? `${introduce}\n\n` : "";
+                        descr += "\n【配置需求】\n\n";
+                        descr += sysreq ? `${sysreq.join("\n\n")}\n\n` : "";
+                        descr += "\n【游戏截图】\n\n";
+                        descr += screenshot ? `${screenshot.map(i => `[img]${i}[/img]`).join("\n")}` : "";
+
+                        Gen.output(descr);
+                    }
+
+                    // 提前向SteamCN数据库请求中文名， 感谢 @Deparsoul 提供帮助与授权
+                    $.ajax({
+                        url: `https://steamdb.steamcn.com/app/${steam_id}/data.js?v=38`,
+                        dataType: "text",  // 以“text”形式解析，防止以jsonp格式解析难以使用
+                        success: data1 => {
+                            let jsonp = JSON.parse(data1.match(/proc\((.+)\)/)[1]);
+                            cname = jsonp.name_cn;
+                            descriptionGenerator()
+                        },
+                    });
+
+                    // 定位信息
+                    let parser = html_parser(data);
+                    let descr_anchor = parser.page.find("div#game_area_description");  // 游戏简介
+
+                    // 开始解析页面信息
+                    cover = (parser.page.find("img.game_header_image_full").attr("src") || "")
+                        .replace(/^(.+?)(\?t=\d+)?$/,"$1");    // 游戏封面图
+
+                    name = parser.page.find("div.apphub_AppName").text().trim();   // 游戏名(英文)
+                    genre = parser.page.find("div.details_block a[href*='/genre/']").map((i,item) => {
+                        return $(item).text().trim();
+                    }).get();  // 类型
+                    developer = parser.page.find("div.details_block  a[href*='/search/?developer=']").map((i,item) => {
+                        return $(item).text().trim();
+                    }).get();  // 开发商
+                    publisher = parser.page.find("div.details_block  a[href*='/search/?publisher=']").map((i,item) => {
+                        return $(item).text().trim();
+                    }).get();  // 发行商
+                    date = parser.page.find("div.release_date > div.date").text().trim();  // 发行日期
+                    linkbar = (parser.page.find("a.linkbar:contains('访问网站')").attr("href") || "")
+                        .replace(/^.+?url=(.+)$/,"$1");     // 官网
+                    language = parser.page.find("table.game_language_options > tbody > tr:gt(0)").map((i,item) => {
+                        const lag_checkcol_list = ["界面", "完全音频", "字幕"];
+                        let tag = $(item);
+                        if (tag.text().match(/不支持/)) return null;
+                        else {
+                            let lag_td = tag.find("td");
+                            let lag_text = lag_td.eq(0).text().trim();
+                            let lag_support_type = [];
+                            for (let i=0;i<3;i++) {
+                                if (lag_td.eq(i).find("img")) lag_support_type.push(lag_checkcol_list[i]);
+                            }
+                            return `${lag_text}(${lag_support_type})`
+                        }
+                    }).get().slice(0,5);
+                    tags = parser.page.find("a.app_tag").map((i,item) => {
+                        return $(item).text().trim();
+                    }).get().slice(0,9);  // 标签
+                    review = parser.page.find("div.user_reviews_summary_row").map((i,item) => {
+                       let tag = $(item);
+                       let review_type = tag.find("div.subtitle").text().trim().replace(/[:：]/,"");
+                       let review_descr1 = tag.find("span.game_review_summary").text().trim();
+                       let review_descr2 = tag.attr("data-tooltip-text").trim();
+                       return `${review_type}： ${review_descr1} （${review_descr2}） `
+
+                    }).get();
+
+                    introduce = descr_anchor.text().replace("关于这款游戏","").trim();
+
+                    let os_dict = {"win": "Windows", "mac": "Mac OS X", "linux": "SteamOS + Linux"};
+                    sysreq = parser.page.find("div.sysreq_contents > div.game_area_sys_req").map((i,item) => {
+                        let tag = $(item);
+                        let os_type = os_dict[tag.attr("data-os")];
+                        let os_req = tag.html().replace('<div style="clear: both;"></div>',"")
+                            .replace(/<br>/ig,"\n").replace(/<[^>]+?>/ig,"").trim()
+                            .split("\n").map(i => i.trim()).join("\n");
+
+                        return `${os_type}\n${os_req}`
+                    }).get();
+
+                    screenshot = parser.page.find("div.screenshot_holder a").map((i,item) => {
+                        return $(item).attr("href").replace(/^.+?url=(http.+?)\.[\dx]+(.+?)(\?t=\d+)?$/,"$1$2")
+                    }).get();  // 游戏截图
+
+                    descriptionGenerator()
+                }
+
+                $.ajax({
+                    type: "get",
+                    url: `http://store.steampowered.com/app/${steam_id}/`, // 重写请求链接
+                    success: data => {
+                        if (/(欢迎来到|Welcome to) Steam/.test(data)) {
+                            system.showErrorMessage("Steam 上不存在对应资源");
+                            return;
+                        }
+                        if (/DoAgeGateSubmit\(\)/.test(data)) {
+                            chrome.cookies.get({
+                                url: "https://store.steampowered.com/",
+                                name: "sessionid",
+                            },(cookies) => {
+                                let sessionid = cookies.value;
+                                $.ajax({
+                                    type: "post",
+                                    url: "http://store.steampowered.com/agecheck/app/${steam_id}/",
+                                    data: {
+                                        "snr": "1_agecheck_agecheck__age-gate",
+                                        "sessionid": sessionid,
+                                        "ageDay": 1,
+                                        "ageMonth": "January",
+                                        "ageYear": "1979"
+                                    },
+                                    success: data1 => {
+                                        steam_parser(data1);
+                                    }
+                                });
+                            });  // 避免 Steam 年龄认证（年龄选择类） 做准备
+                            return;
+                        }
+
+                        steam_parser(data);
+                    }
+                })
 
 
             }  // TODO Steam链接
@@ -375,8 +531,14 @@
         },    // TODO 使用远程API服务器
 
         gen(search_func) {
+            let search_key = Gen.key();
+            if (search_key.length === 0) {
+                system.showErrorMessage("未输入搜索内容");
+                return;
+            }
+
             search_func = search_func || Gen.search.douban;
-            if (/^http/.test(Gen.key())) {  // 识别为链接格式
+            if (/^http/.test(search_key)) {  // 识别为链接格式
                 $("#gen-help").hide();   // 显示帮助表格
                 $("#gen-out").show();   // 隐藏输出窗口
                 system.showMessage("识别输入内容为链接格式，开始请求源数据....");
